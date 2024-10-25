8d0e233b35414876956db30898e18920714b4084
Karen Arutyunov <karen@codesynthesis.com>
2024-10-25 14:57:08 +0200

    Add more regexes

--- hello/hello.cxx
+++ hello/hello.cxx
@@ -9,23 +9,23 @@ int main (int argc, char* argv[])
   {
     regex re(".*");
 
-    cout << "'a' '.*' 'x': '" << regex_replace("a", re, "x") << "'" << endl;
+//    cout << "'a' '.*' 'x': '" << regex_replace("a", re, "x") << "'" << endl;
     cout << "'a' '.*' 'x' (match_not_null): '" << regex_replace("a", re, "x", regex_constants::match_not_null) << "'" << endl;
     cout << endl;
 
     cout << "'' '.*' 'x': '" << regex_replace("", re, "x") << "'" << endl;
-    cout << "'' '.*' 'x' (match_not_null): '" << regex_replace("", re, "x", regex_constants::match_not_null) << "'" << endl;
+//    cout << "'' '.*' 'x' (match_not_null): '" << regex_replace("", re, "x", regex_constants::match_not_null) << "'" << endl;
     cout << endl;
   }
 
   {
     regex re("a*");
 
-    cout << "'bb' 'a*' 'x': '" << regex_replace("bb", re, "x") << "'" << endl;
+//    cout << "'bb' 'a*' 'x': '" << regex_replace("bb", re, "x") << "'" << endl;
     cout << "'bb' 'a*' 'x' (match_not_null): '" << regex_replace("bb", re, "x", regex_constants::match_not_null) << "'" << endl;
     cout << endl;
 
-    cout << "'b' 'a*' 'x': '" << regex_replace("b", re, "x") << "'" << endl;
+//    cout << "'b' 'a*' 'x': '" << regex_replace("b", re, "x") << "'" << endl;
     cout << "'b' 'a*' 'x' (match_not_null): '" << regex_replace("b", re, "x", regex_constants::match_not_null) << "'" << endl;
   }
 }
