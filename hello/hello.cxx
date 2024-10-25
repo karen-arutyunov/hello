e5169a0298c0a7c09bb9b5f19756c1f3ec23dd68
Karen Arutyunov <karen@codesynthesis.com>
2024-10-25 11:10:48 +0200

    Add more regexes

--- hello/hello.cxx
+++ hello/hello.cxx
@@ -6,8 +6,22 @@ int main (int argc, char* argv[])
 {
   using namespace std;
 
-  regex re(".*");
-  cout << regex_replace("a", re, "x") << endl;
-  cout << regex_replace("a", re, "x", regex_constants::match_not_null) << endl;
-  cout << regex_replace("", re, "x") << endl;
+  {
+    regex re(".*");
+
+    cout << "'a' '.*' 'x': '" << regex_replace("a", re, "x") << "'" << endl;
+    cout << "'a' '.*' 'x' (match_not_null): '" << regex_replace("a", re, "x", regex_constants::match_not_null) << "'" << endl;
+    cout << endl;
+
+    cout << "'' '.*' 'x': '" << regex_replace("", re, "x") << "'" << endl;
+    cout << "'' '.*' 'x' (match_not_null): '" << regex_replace("", re, "x", regex_constants::match_not_null) << "'" << endl;
+    cout << endl;
+  }
+
+  {
+    regex re("a*");
+
+    cout << "'bb' 'a*' 'x': '" << regex_replace("bb", re, "x") << "'" << endl;
+    cout << "'bb' 'a*' 'x' (match_not_null): '" << regex_replace("bb", re, "x", regex_constants::match_not_null) << "'" << endl;
+  }
 }
